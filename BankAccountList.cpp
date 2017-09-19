793f0e3150ba483676d8f939db33a6c3aca83730
plushy1anthony999 <anthonyghouri@gmail.com>
2017-09-19 02:44:06 -0700

    Moved all contents into new folder "Assignment 2 Part 2"

--- BankAccountList.cpp
+++ BankAccountList.cpp
@@ -1,5 +1,7 @@
 #include "BankAccountList.h"
 
+using namespace std;
+
 BankAccountList::BankAccountList() {
 
 }
--- BankAccountList.h
+++ BankAccountList.h
@@ -1,7 +1,24 @@
 #pragma once
+#include <iostream>
+#include "BankAccount.h"
+
+// Tells whether the List array	in BankAccountList Class is currently sorted
+enum class LIST_STATE_FLAGS { UNSORTED, SORTED_BY_ACCOUNT, SORTED_BY_LASTNAME, SORTED_BY_BALANCE }; 
+
 
 class BankAccountList {
+	const static int MAX = 30;
+
+	BankAccount List[MAX];
+	int list_state; 
+	int num_elements; // Number of active elelments in List[] i.e. its logical length
+
 public:
 	BankAccountList();
+
+	// Getters
+	const std::string getFirstName(const std::string & actNum) const;
+	const std::string getLastName(const std::string & actNum) const;
+	const std::string getFullName(const std::string & actNum) const;
 };
 
